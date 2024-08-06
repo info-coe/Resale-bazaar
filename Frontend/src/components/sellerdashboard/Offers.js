f581425e85625a7b1860d5db008baa0349ba8b2c
info-coe <coeteam@infomericainc.in>
2024-08-06 17:46:45 +0530

    updated

--- Frontend/src/components/sellerdashboard/Offers.js
+++ Frontend/src/components/sellerdashboard/Offers.js
@@ -3,46 +3,62 @@ import axios from 'axios';
 import MyNavbar from '../navbar';
 import Footer from '../footer';
 import Scrolltotopbtn from '../Scrolltotopbutton';
+// import InfiniteScroll from "react-infinite-scroll-component";
+import OfferedProduct from './offeredproduct';
+
 
 const Offers = () => {
     const [offers, setOffers] = useState([]);
+    const [approvedOffers, setApprovedOffers] = useState([]);
     const userToken = sessionStorage.getItem("user-token");
-
     useEffect(() => {
         axios.get(`${process.env.REACT_APP_HOST}${process.env.REACT_APP_PORT}/sellerproductsoffers`)
         .then((response) => {
             if (response.data !== "Fail" && response.data !== "Error") {
                 setOffers(response.data);
+                const customerApprovedOffers = response.data.filter(offer =>
+                    offer.offered_buyer_id.toString() === userToken && offer.product_status === 'Accepted'
+                );
+                setApprovedOffers(customerApprovedOffers);
             }
-        }).catch((err) => {
+        })
+        .catch((err) => {
             console.log('error', err);
         });
-    }, []);
+    }, [userToken]);
 
-    const handleAccept = async (offerId) => {
-        try {
-            const response = await axios.post(`${process.env.REACT_APP_HOST}${process.env.REACT_APP_PORT}/offers/${offerId}/accept`);
+    const handleAccept = (offerId) => {
+        axios.post(`${process.env.REACT_APP_HOST}${process.env.REACT_APP_PORT}/offers/${offerId}/accept`)
+        .then((response) => {
             const updatedOffer = response.data;
             setOffers(offers.map(offer => (offer.id === updatedOffer.id ? { ...offer, product_status: 'Accepted' } : offer)));
+
+            if (updatedOffer && updatedOffer.offered_buyer_id && updatedOffer.offered_buyer_id.toString() === userToken) {
+                setApprovedOffers([...approvedOffers, updatedOffer]);
+            }
             window.location.reload(false);
-        } catch (error) {
+        })
+        .catch((error) => {
             console.error('Error accepting offer:', error);
-        }
+        });
     };
 
-    const handleReject = async (offerId) => {
-        try {
-            const response = await axios.post(`${process.env.REACT_APP_HOST}${process.env.REACT_APP_PORT}/offers/${offerId}/reject`);
+    const handleReject = (offerId) => {
+        axios.post(`${process.env.REACT_APP_HOST}${process.env.REACT_APP_PORT}/offers/${offerId}/reject`)
+        .then((response) => {
             const updatedOffer = response.data;
             setOffers(offers.map(offer => (offer.id === updatedOffer.id ? { ...offer, product_status: 'Rejected' } : offer)));
             window.location.reload(false);
-        } catch (error) {
+        })
+        .catch((error) => {
             console.error('Error rejecting offer:', error);
-        }
+        });
     };
 
     const sellerOffers = offers.filter(offer => offer.seller_id.toString() === userToken);
     const customerOffers = offers.filter(offer => offer.offered_buyer_id.toString() === userToken);
+
+    
 
     const renderOffersTable = (offers, isSellerOffers) => (
         <div className="table-responsive mt-4">
@@ -53,7 +69,7 @@ const Offers = () => {
                         <th>Image</th>
                         <th>Offer Amount</th>
                         <th>Status</th>
-                        {isSellerOffers && <th>Actions</th>}
+                        {isSellerOffers && offers.product_status === 'Pending' && (<th>Actions</th>)}
                     </tr>
                 </thead>
                 <tbody>
@@ -84,6 +100,36 @@ const Offers = () => {
         </div>
     );
 
+    const renderApprovedOffersCards = (offers) => (
+        // <InfiniteScroll
+        //     dataLength={offers.length}
+        //     next={() => setPage((prevPage) => prevPage + 1)}
+        //     hasMore={hasMore}
+        //     loader={
+        //         <div className="centered-message">
+        //             <i className="bi bi-arrow-clockwise spin-icon"></i>
+        //         </div>
+        //     }
+        //     endMessage={
+        //         <div className="centered-message">
+        //             <p>No more products to display</p>
+        //         </div>
+        //     }
+        // >
+            <div className="product-grid container">
+                {offers.length > 0 ? (
+                    offers.map((offer) => (
+                        <div key={offer.id} className='pt-4'>
+                            <OfferedProduct product={offer}/>
+                        </div>
+                    ))
+                ) : (
+                    <h2 style={{ fontSize: "18px" }}>No products to display</h2>
+                )}
+            </div>
+        // </InfiniteScroll>
+    );
+
     return (
         <div className='fullscreen'>
             <MyNavbar />
@@ -91,36 +137,38 @@ const Offers = () => {
                 <div className="container mt-4">
                     <h1 className="mb-4 text-center">Offers</h1>
 
-                    {(sellerOffers.length > 0 || customerOffers.length > 0) ? (
-                        sellerOffers.length > 0 && customerOffers.length > 0 ? (
-                            <div>
-                                <ul className="nav nav-tabs" id="offersTabs" role="tablist">
-                                    <li className="nav-item" role="presentation">
-                                        <button className="nav-link active" id="seller-offers-tab" data-bs-toggle="tab" data-bs-target="#seller-offers" type="button" role="tab" aria-controls="seller-offers" aria-selected="true">Seller Offers</button>
-                                    </li>
-                                    <li className="nav-item" role="presentation">
-                                        <button className="nav-link" id="customer-offers-tab" data-bs-toggle="tab" data-bs-target="#customer-offers" type="button" role="tab" aria-controls="customer-offers" aria-selected="false">Customer Offers</button>
-                                    </li>
-                                </ul>
-                                <div className="tab-content" id="offersTabsContent">
-                                    <div className="tab-pane fade show active" id="seller-offers" role="tabpanel" aria-labelledby="seller-offers-tab">
-                                        {renderOffersTable(sellerOffers, true)}
-                                    </div>
-                                    <div className="tab-pane fade" id="customer-offers" role="tabpanel" aria-labelledby="customer-offers-tab">
-                                        {renderOffersTable(customerOffers, false)}
-                                    </div>
+                    {(sellerOffers.length > 0 || customerOffers.length > 0 || approvedOffers.length > 0) ? (
+                        <div>
+                            <ul className="nav nav-tabs" id="offersTabs" role="tablist">
+                                <li className="nav-item" role="presentation">
+                                    <button className="nav-link active" id="seller-offers-tab" data-bs-toggle="tab" data-bs-target="#seller-offers" type="button" role="tab" aria-controls="seller-offers" aria-selected="true">Incoming Requests</button>
+                                </li>
+                                <li className="nav-item" role="presentation">
+                                    <button className="nav-link" id="customer-offers-tab" data-bs-toggle="tab" data-bs-target="#customer-offers" type="button" role="tab" aria-controls="customer-offers" aria-selected="false">Outgoing Requests</button>
+                                </li>
+                                <li className="nav-item" role="presentation">
+                                    <button className="nav-link" id="approved-offers-tab" data-bs-toggle="tab" data-bs-target="#approved-offers" type="button" role="tab" aria-controls="approved-offers" aria-selected="false">Approved Offers</button>
+                                </li>
+                            </ul>
+                            <div className="tab-content" id="offersTabsContent">
+                                <div className="tab-pane fade show active" id="seller-offers" role="tabpanel" aria-labelledby="seller-offers-tab">
+                                    {renderOffersTable(sellerOffers, true)}
+                                </div>
+                                <div className="tab-pane fade" id="customer-offers" role="tabpanel" aria-labelledby="customer-offers-tab">
+                                    {renderOffersTable(customerOffers, false)}
+                                </div>
+                                <div className="tab-pane fade" id="approved-offers" role="tabpanel" aria-labelledby="approved-offers-tab">
+                                    {renderApprovedOffersCards(approvedOffers)}
                                 </div>
                             </div>
-                        ) : (
-                            renderOffersTable(sellerOffers.length > 0 ? sellerOffers : customerOffers, sellerOffers.length > 0)
-                        )
+                        </div>
                     ) : (
                         <div className="text-center">No offers found.</div>
                     )}
                 </div>
             </main>
             <Footer />
-            <Scrolltotopbtn/>
+            <Scrolltotopbtn />
         </div>
     );
 };
---
+++ Frontend/src/components/sellerdashboard/offeredproduct.js
@@ -0,0 +1,239 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Carousel from "react-bootstrap/Carousel";
+import { useCart } from "../CartContext";
+
+const truncateName = (name, maxLength) => {
+  if (name.length > maxLength) {
+    return name.substring(0, maxLength) + "...";
+  }
+  return name;
+};
+
+const OfferedProduct = (props) => {
+  // eslint-disable-next-line no-unused-vars
+  const [existingProducts, setExistingProducts] = useState([]);
+  // eslint-disable-next-line no-unused-vars
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [carouselIndex, setCarouselIndex] = useState({});
+  const [sliding, setSliding] = useState({});
+  const [showIndicators, setShowIndicators] = useState({});
+  const [userDetails, setUserDetails] = useState([]);
+  const {addToCart} = useCart()
+
+
+  props.product.userid = sessionStorage.getItem("user-token");
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_HOST}${process.env.REACT_APP_PORT}/addcart`)
+      .then((response) => {
+        if (response.data !== "Fail" && response.data !== "Error") {
+          if (Array.isArray(response.data)) {
+            setExistingProducts(response.data);
+          }
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching cart items:", error);
+      });
+
+    if (sessionStorage.getItem("token") !== "admin") {
+      sessionStorage.getItem("user-token") !== null && setIsLoggedIn(true);
+    }
+
+    axios
+      .post(
+        `${process.env.REACT_APP_HOST}${process.env.REACT_APP_PORT}/users`,
+        { sellerID: props.product.seller_id }
+      )
+      .then((res) => {
+        if (res.data !== "Fail" && res.data !== "Error") {
+          const userDetails = res.data.map((item) => ({
+            userId: item.user_id,
+            email: item.email,
+            phone: item.phone,
+            name: item.firstname + " " + item.lastname,
+            shopname: item.shopname,
+          }));
+          setUserDetails(userDetails);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching seller details:", error);
+      });
+  }, [props.product.image, props.product.seller_id]);
+
+  const datta = JSON.parse(props.product.image);
+
+  const handleMouseEnter = (id) => {
+    setShowIndicators((prevState) => ({
+      ...prevState,
+      [id]: true,
+    }));
+    setSliding((prevState) => ({
+      ...prevState,
+      [id]: true,
+    }));
+  };
+
+  const handleMouseLeave = (id) => {
+    setShowIndicators((prevState) => ({
+      ...prevState,
+      [id]: false,
+    }));
+    setSliding((prevState) => ({
+      ...prevState,
+      [id]: false,
+    }));
+    setCarouselIndex((prevState) => ({
+      ...prevState,
+      [id]: 0,
+    }));
+  };
+
+  const handleSelect = (selectedIndex, id) => {
+    setCarouselIndex((prevState) => ({
+      ...prevState,
+      [id]: selectedIndex,
+    }));
+  };
+
+  const getMaxLength = () => {
+    if (window.innerWidth <= 575.98) {
+      return 19;
+    }
+    return 25;
+  };
+
+  const [maxLength, setMaxLength] = useState(getMaxLength);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setMaxLength(getMaxLength());
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const truncatedName = truncateName(props.product.name, maxLength);
+
+  return (
+    <div className="d-flex justify-content-center">
+      <div
+        className="card productcard product-card"
+        style={{ opacity: props.product.quantity === 0 ? 0.5 : 1 }}
+      >
+    
+          <div
+            className="text-center productimgback position-relative"
+            onMouseEnter={() => handleMouseEnter(props.product.id)}
+            onMouseLeave={() => handleMouseLeave(props.product.id)}
+          >
+            <Carousel
+              activeIndex={carouselIndex[props.product.id] || 0}
+              interval={sliding[props.product.id] ? 1000 : null}
+              controls={false}
+              indicators={showIndicators[props.product.id] || false}
+              onSelect={(selectedIndex) =>
+                handleSelect(selectedIndex, props.product.id)
+              }
+              pause={false}
+            >
+              {datta.map((item, index) => (
+                <Carousel.Item key={index} className="carousel-item-custom">
+                  {item.endsWith(".mp4") ||
+                  item.endsWith(".webm") ||
+                  item.endsWith(".mov") ||
+                  item.endsWith(".avi") ||
+                  item.endsWith(".quicktime") ? (
+                    <video
+                      src={item}
+                      className="d-block w-100"
+                      controls
+                      muted
+                      height="220px"
+                      alt={`product-video-${index}`}
+                      style={{ objectFit: "contain" }}
+                    >
+                      <source src={item} type="video/mp4" />
+                      <source src={item} type="video/webm" />
+                      <source src={item} type="video/quicktime" />
+                      <source src={item} type="video/avi" />
+                      <source src={item} type="video/mov" />
+                      Your browser does not support the video tag.
+                    </video>
+                  ) : (
+                    <img
+                      src={item}
+                      className="d-block w-100"
+                      alt={`product-${index}`}
+                      style={{ height: "220px", objectFit: "cover" }}
+                    />
+                  )}
+                </Carousel.Item>
+              ))}
+            </Carousel>
+            {props.product.quantity === 0 && (
+             <div
+             className="position-absolute top-50 start-50 translate-middle text-white bg-black px-2 py-1 rounded"
+             style={{ fontSize: "1.2rem", fontWeight: "bold" }}
+           >
+           Sold
+           </div>
+           
+            )}
+          </div>
+        <div className="card-body">
+          {userDetails.length > 0 && (
+            <p
+              className="card-text fw-bold"
+              style={{
+                lineHeight: "20px",
+                marginTop: "-5px",
+                fontSize: "14px",
+                color: "rgb(86, 101, 115 )",
+              }}
+            >
+              {userDetails[0].shopname}
+            </p>
+          )}
+          <p
+            style={{
+              fontSize: "13px",
+              marginTop: "-10px",
+              color: "rgb(93, 109, 126 )",
+            }}
+          >
+            {truncatedName}
+          </p>
+
+          <p
+            className="card-text"
+            style={{ lineHeight: "15px", marginTop: "-8px" }}
+          >
+            <b>&#36; {props.product.offered_price}.00</b>
+          </p>
+          <div className="text-center">
+          <button
+                                onClick={() => {
+                                    props.product.price = props.product.offered_price
+                                     props.product.id = props.product.product_id
+                                    addToCart(props.product, "main")
+                                }}
+                                className="btn btn-sm btn-secondary"
+                            >
+                                Add to Cart
+                            </button>
+                            </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OfferedProduct;
